1a65fcdf12802313ca312a060a7fefcb8ebd704c
Tsedenawit <tsedenawit3@gmail.com>
2025-07-20 00:43:26 -0400

    dated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,7 +5,7 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button styles="red-5oo"/>
+      <Button styles="rounded-sm, rounded-md, rounded-full"/>
     </div>
   )
 }
